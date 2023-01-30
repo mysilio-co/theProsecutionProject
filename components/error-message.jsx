edeca881eb4fb1e82afec39fd4331d5ea966888b
lucas-harris <lute.harris@gmail.com>
2023-01-30 18:36:10 -0500

    updating error message copy

--- components/error-message.jsx
+++ components/error-message.jsx
@@ -12,7 +12,7 @@ export default function ErrorMessage() {
     return (
       <div className="container mx-auto w-5/6 md:w-1/2 bg-gray-800 py-6 my-6 rounded-lg">
         <h2 className="text-xl text-center text-white p-3">An error occurred!</h2>
-        <p className="text-md text-center text-white p-3">Something went wrong! Try refreshing the page, if that doesn't work you can reach out to us by clicking the "Contact Us" button above or send an email to contact-us-email@tpp.gov</p>
+        <p className="text-md text-center text-white p-3">Something went wrong! Try refreshing the page, changing the tab, or resetting the search. If that doesn't work you can reach out to us by clicking the "Contact Us" button above or send an email to contact-us-email@tpp.gov</p>
       </div>
     );
   }
