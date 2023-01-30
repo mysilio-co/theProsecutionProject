da3f6dfd55be2ef655bef52ba47395be5dc7f1a0
lucas-harris <lute.harris@gmail.com>
2023-01-30 15:53:53 -0500

    testing

--- pages/index.jsx
+++ pages/index.jsx
@@ -70,7 +70,8 @@ export default function DataExplorer() {
     const lengthOfSheet = dateColumn ? dateColumn.length : null;
     const { data: firstHalf, error: firstHalfError } = useSWR(locationOfYear && !hasError ? '/api/sheets/getSheets?sheet='+sheet+'&range='+viewType+'&start='+1+'&end='+(locationOfYear-1) : null, fetcher);
     const { data: secondHalf, error: secondHalfError } = useSWR(locationOfYear && lengthOfSheet && !hasError ? '/api/sheets/getSheets?sheet='+sheet+'&range='+viewType+'&start='+(locationOfYear-1)+'&end='+lengthOfSheet : null, fetcher);
-    updateHasError(firstHalfError|| secondHalfError);
+    console.log(secondHalfError);
+    updateHasError(firstHalfError || secondHalfError);
     return firstHalf && secondHalf && !hasError ? firstHalf.concat(secondHalf) : null;
   }
   
