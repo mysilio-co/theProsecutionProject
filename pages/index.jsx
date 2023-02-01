4dde0d4b100464ed18db515e3bb7a77b0c76053d
lucas-harris <lute.harris@gmail.com>
2023-02-01 08:44:24 -0500

    adding socials to footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -203,14 +203,47 @@ export default function DataExplorer() {
           </a>
         </div>
       </div>
-      <div className="relative z-0 flex-1 px-2 pt-6 pb-6 flex items-center justify-center sm:inset-0 bg-gray-800">
-        <img
-          className="block h-24"
-          src="https://i0.wp.com/theprosecutionproject.org/wp-content/uploads/2020/08/Illustration-Hiking-Website-Email-Header-2.png?w=600&ssl=1"
-          alt="The Prosecution Project"
-        />
+      <div className="flex-1 px-2 pt-6 pb-3 flex items-center justify-center sm:inset-0 bg-gray-800">
+        <div className="relative">
+          <a href="https://theprosecutionproject.org/" target="_blank">
+            <img
+              className="block h-24"
+              src="https://i0.wp.com/theprosecutionproject.org/wp-content/uploads/2020/08/Illustration-Hiking-Website-Email-Header-2.png?w=600&ssl=1"
+              alt="The Prosecution Project"
+            />
+          </a>
+        </div>
       </div>
-
+      <div className="flex-1 px-2 pt-3 pb-6 flex items-center justify-center sm:inset-0 bg-gray-800">
+        <div className="mx-5">
+          <a href="mailto:michael@theprosecutionproject.org" target="_blank">
+            <img
+              className="block h-10"
+              src="/mail.png"
+              alt="email icon"
+            />
+          </a>
+        </div>
+        <div className="mx-5">
+          <a href="https://twitter.com/ProsecutionProj" target="_blank">
+            <img
+              className="block h-10"
+              src="/twitter.png"
+              alt="twitter logo"
+            />
+          </a>
+        </div>
+        <div className="mx-5">
+          <a href="https://www.linkedin.com/company/the-prosecution-project/" target="_blank">
+            <img
+              className="block h-10"
+              src="/linkedin.png"
+              alt="linkedin logo"
+            />
+          </a>
+        </div>
+      </div>
+      
     </>
   );
 }
