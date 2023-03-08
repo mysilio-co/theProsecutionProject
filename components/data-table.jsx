6c040513ea5a91af65c0b97829a217f58b95ac82
lucas-harris <lute.harris@gmail.com>
2023-03-08 16:44:24 -0500

    more column description styling, still not completely settled on it but getting close

--- components/data-table.jsx
+++ components/data-table.jsx
@@ -55,7 +55,7 @@ export default function DataTable({ title, data, length, router, isLoading, isMo
             </p>
             <div className="md:flex md:justify-between items-center mt-6">
               <div className="flex items-center">
-              <p className={classNames((showColumnDescription ? 'opacity-100' : 'opacity-0'), "absolute bg-white text-sm text-gray-700 border border-gray-400 rounded p-5 z-50 transition-opacity ease-in-out duration-300")}>
+              <p className={classNames((showColumnDescription ? 'opacity-100' : 'opacity-0 invisible'), "absolute left-8 right-8 bg-white text-sm text-gray-700 border border-gray-800 rounded p-5 z-50 transition-opacity ease-in-out duration-300")}>
                 {columnDescription + ": " + CODEBOOK[columnDescription]}
               </p>
                 <p className="text-lg font-semibold text-gray-700">
