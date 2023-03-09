9c2150d314683ca2b8452c6d1e029ae50bb9f5e9
lucas-harris <lute.harris@gmail.com>
2023-03-09 10:40:42 -0500

    changing copy of error message

--- components/error-message.jsx
+++ components/error-message.jsx
@@ -2,7 +2,7 @@ export default function ErrorMessage() {
     return (
       <div className="container mx-auto w-5/6 md:w-1/2 bg-gray-800 py-6 my-6 rounded-lg">
         <h2 className="text-xl text-center text-white p-3">An error occurred!</h2>
-        <p className="text-md text-center text-white p-3">Something went wrong! Try refreshing the page, changing the tab, or resetting the search. If that doesn't work, you can reach out to us directly through our email or socials below.</p>
+        <p className="text-md text-center text-white p-3">Something went wrong! Try refreshing the page, changing the tab, or resetting the search. If that doesn't work, you can reach out to us directly by clicking the "Contact Us" button below.</p>
       </div>
     );
   }
