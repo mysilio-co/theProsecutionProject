efecf178b9d0ca603c6703260af812f6e984229e
lucas-harris <lute.harris@gmail.com>
2023-05-09 11:50:21 -0400

    adding i understand text

--- components/modals/in-progress-modal-contents.jsx
+++ components/modals/in-progress-modal-contents.jsx
@@ -17,6 +17,9 @@ export default function InProgressModalContents ({setShowModal}) {
             <div className="bg-white pt-0 pb-4 sm:pb-4">
                 <div className="sm:flex sm:items-start">
                     <div className="mt-0 text-center sm:text-left w-full">
+                    <Dialog.Title  as="h3" className="p-4 bg-gray-800 text-lg font-medium leading-6 text-white">
+                            In Progress Tab Disclaimer
+                        </Dialog.Title>
                         <div className="mt-2 p-4 pb-0 text-center">
                             <h2>Attention!</h2>
                             <p className="mt-6 text-sm text-gray-500">
@@ -33,7 +36,7 @@ export default function InProgressModalContents ({setShowModal}) {
                     className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-100 focus:outline-blue sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setModalVisibility(false)}
                 >
-                    Close
+                    I understand
                 </button>
             </div>
         </Dialog.Panel>
