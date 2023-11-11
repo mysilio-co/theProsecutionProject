e4ca4538949ea9fceb07b28c5d98038640cc0154
lucas-harris <lute.harris@gmail.com>
2023-11-11 11:58:33 -0500

    changing cursor on active filter checkbox when disabled

--- components/modals/data-visualizer/show-filter-checkbox.jsx
+++ components/modals/data-visualizer/show-filter-checkbox.jsx
@@ -9,8 +9,10 @@ export default function ShowActiveFilterCheckbox({
     <div className='flex ml-6 mb-1 items-center'>
       <label
         className={classNames(
-          !isActiveFilters ? 'opacity-50' : 'opacity-100',
-          'inline-flex relative items-center cursor-pointer  disabled:text-slate-100',
+          !isActiveFilters
+            ? 'opacity-50 cursor-not-allowed'
+            : 'opacity-100 cursor-pointer ',
+          'inline-flex relative items-center disabled:text-slate-100',
         )}
       >
         <input
