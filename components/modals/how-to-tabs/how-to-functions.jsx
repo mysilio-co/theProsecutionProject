cf4fc92e20807988a4b61633083d53170163de3a
lucas-harris <lute.harris@gmail.com>
2025-07-20 12:29:38 -0400

    removing console logs

--- components/modals/how-to-tabs/how-to-functions.jsx
+++ components/modals/how-to-tabs/how-to-functions.jsx
@@ -14,8 +14,6 @@ import HowToFunctionsButtons from './how-to-functions/how-to-functions-button';
 
 export default function HowToFunctions({ isMobile }) {
   const [selectedFunction, setSelectedFunction] = useState(DATASET);
-  console.log(isMobile);
-
   return (
     <div>
       <div className='bg-gray-800 border-t border-gray-700'>
